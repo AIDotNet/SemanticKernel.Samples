2607c3e7648f5252887771fe94ec4fc87b7a624d
token <61819790+239573049@users.noreply.github.com>
2025-04-27 11:22:43 +0800

    初始化项目

---
+++ src/Samples1/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.SemanticKernel;
+
+#pragma warning disable SKEXP0010
+
+var kernel = Kernel.CreateBuilder()
+    .AddOpenAIChatCompletion("gpt-4.1-mini", new Uri("https://api.token-ai.cn/v1"),
+        "您的密钥")
+    .Build();
+
+await foreach (var item in kernel.InvokePromptStreamingAsync("您好，我是TokenAI"))
+{
+    Console.Write(item.ToString());
+};
---
+++ src/Samples2/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+#pragma warning disable SKEXP0010
+
+var kernel = Kernel.CreateBuilder()
+    .AddOpenAIChatCompletion("gpt-4.1-mini", new Uri("https://api.token-ai.cn/v1"),
+        "您的密钥")
+    .Build();
+
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+ChatHistory history = [];
+
+history.AddUserMessage("Hello, how are you?");
+
+// 使用同步对话
+var response = await chatCompletionService.GetChatMessageContentAsync(
+    history,
+    kernel: kernel
+);
+
+Console.WriteLine(response.Content);
+
+
+// 使用异步对话
+await foreach (var item in chatCompletionService.GetStreamingChatMessageContentsAsync(
+                   history,
+                   kernel: kernel
+               ))
+{
+    Console.Write(item.Content);
+}
